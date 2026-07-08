3462fa4ababa92ea0d2d1e20b9286d83fccc2cee
sukh767 <sukharanjanjana767@gmail.com>
2026-07-09 00:36:13 +0530

    feat(auth): create shared authentication store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,25 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import useAuthStore from './stores/auth.store.js';
-import {
-    setAccessToken,
-    getAccessToken,
-    clearAccessToken,
-} from "@scan/api";
+import React from 'react'
+import { useAuthStore } from "@scan/auth";
 
-import { ApiError } from "@scan/api";
+function Test() {
 
-const error = new ApiError({
-    message: "Unauthorized",
-    statusCode: 401,
-});
+    const {
+        user,
+        isAuthenticated,
+        setUser,
+        clearUser,
+    } = useAuthStore();
 
-console.log(error);
+    console.log(user);
+    console.log(isAuthenticated);
 
-import { request } from "@scan/api";
-
-import { queryClient } from "@scan/query";
-
-console.log(queryClient);
-
-console.log(typeof request.get);
-console.log(typeof request.post);
-console.log(typeof request.put);
-console.log(typeof request.patch);
-console.log(typeof request.delete);
-
-import { env } from "@scan/config";
-// Protected route wrapper
-const ProtectedRoute = ({ children }) => {
-  const user = useAuthStore((s) => s.user);
-  return user ? children : <Navigate to="/login" replace />;
-};
-
-
-setAccessToken("abc123");
-
-console.log(getAccessToken());
-
-clearAccessToken();
-
-console.log(getAccessToken());
-
-export default function App() {
-
-console.log(env.apiUrl);
-  return (
-    <Routes>
-      <Route path="/" element={<h1>Scan and Dine</h1>} />
-    </Routes>
-  );
+    return null;
 }
+
+function App() {
+  return (
+    <h1 className='text-2xl text-blue-500 font-bold'>Welcome to Scan and Dine</h1>
+  )
+}
+
+export default App
